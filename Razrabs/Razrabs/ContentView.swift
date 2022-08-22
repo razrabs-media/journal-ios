05a8f280d3b0a0e9421f2e3d0b72a9b709753077
Yevgeniy Zakharov <fnc12@me.com>
2022-08-23 00:52:34 +0600

    feed items scroll view started

--- Razrabs/Razrabs/ContentView.swift
+++ Razrabs/Razrabs/ContentView.swift
@@ -3,14 +3,41 @@ import SwiftUI
 struct ContentView: View {
     let razrabsApi: RazrabsApi
     
+    @State var feedItems = [FeedItem]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .onAppear {
-                razrabsApi.requestFeed(callback: { result in
-                    print("result = \(result)")
-                })
+        NavigationView {
+            VStack {
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(feedItems.indices, id: \.self) { index in
+                            Button {
+                                print("feed item \(feedItems[index].name) selected")
+                            } label: {
+                                Text(feedItems[index].name)
+                                    .background(Color(.lightGray))
+                                    .foregroundColor(Color(.black))
+                                    .padding(2)
+                            }
+                        }
+                    }
+                }
+                    .padding()
+                    .navigationTitle("Разрабы")
+                    .onAppear {
+                        razrabsApi.requestFeed(callback: { result in
+                            print("result = \(result)")
+                            switch result {
+                            case .success(let feedResponse):
+                                feedItems = feedResponse.data.feeds// + feedResponse.data.feeds
+                            case .failure(let error):
+                                print("error = \(error)")
+                            }
+                        })
+                    }
+                Spacer()
             }
+        }
     }
 }
 
