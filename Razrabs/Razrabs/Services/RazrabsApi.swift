94f5f43e499976ff55e985f02e9ad74186deaf2d
Yevgeniy Zakharov <yevgeniyzakharov@MBP-Yevgeniy.hitronhub.home>
2022-10-08 09:46:31 -0400

    refactored api

--- Razrabs/Razrabs/Services/RazrabsApi.swift
+++ Razrabs/Razrabs/Services/RazrabsApi.swift
@@ -15,6 +15,30 @@ class RazrabsApi {
         self.host = host
     }
     
+    private func makeRequest<T: Codable>(body: String, callback: @escaping (_ result: Result<T, Swift.Error>) -> ()) {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = scheme
+        urlComponents.host = host
+        urlComponents.path = "/gql"
+        guard let url = urlComponents.url else {
+            fatalError()
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        urlRequest.addValue("1", forHTTPHeaderField: "DNT")
+        urlRequest.addValue("https://api.razrabs.ru", forHTTPHeaderField: "Origin")
+        urlRequest.httpMethod = "POST"
+        let jsonDictionary = [
+            "query" : body,
+        ]
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: jsonDictionary, options: []) else {
+            fatalError()
+        }
+        urlRequest.httpBody = jsonData
+        makeRequest(urlRequest: urlRequest, callback: callback)
+    }
+    
     private func makeRequest<T: Codable>(urlRequest: URLRequest, callback: @escaping (_ result: Result<T, Swift.Error>) -> ()) {
         let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
             let fireCallback = { (_ result: Result<T, Swift.Error>) -> Void in
@@ -51,74 +75,17 @@ class RazrabsApi {
     }
     
     func requestPost(with uid: String, callback: @escaping (_ result: Result<PostResponse, Swift.Error>) -> Void) {
-        var urlComponents = URLComponents()
-        urlComponents.scheme = scheme
-        urlComponents.host = host
-        urlComponents.path = "/gql"
-        guard let url = urlComponents.url else {
-            fatalError()
-        }
-        var urlRequest = URLRequest(url: url)
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
-        urlRequest.addValue("1", forHTTPHeaderField: "DNT")
-        urlRequest.addValue("https://api.razrabs.ru", forHTTPHeaderField: "Origin")
-        urlRequest.httpMethod = "POST"
-        let jsonDictionary = [
-            "query" : "{post(uid: \"\(uid)\") { uid createdAt updatedAt title readingTime previewUrl content description status tags {uid createdAt updatedAt name description}githubAuthor {uid createdAt updatedAt usernameUrl avatarUrl name postsCount user {uid createdAt updatedAt login isTransportPassword status profileUid profile {uid createdAt updatedAt fullName publicName email avatarUrl commentsCount}userGroups{uid createdAt updatedAt name}}}interactions {sharesCount viewsCount}comments {uid createdAt updatedAt content author {uid createdAt updatedAt fullName publicName avatarUrl commentsCount}content}}}",
-        ]
-        guard let jsonData = try? JSONSerialization.data(withJSONObject: jsonDictionary, options: []) else {
-            fatalError()
-        }
-        urlRequest.httpBody = jsonData
-        makeRequest(urlRequest: urlRequest, callback: callback)
+        makeRequest(body: "{post(uid: \"\(uid)\") { uid createdAt updatedAt title readingTime previewUrl content description status tags {uid createdAt updatedAt name description}githubAuthor {uid createdAt updatedAt usernameUrl avatarUrl name postsCount user {uid createdAt updatedAt login isTransportPassword status profileUid profile {uid createdAt updatedAt fullName publicName email avatarUrl commentsCount}userGroups{uid createdAt updatedAt name}}}interactions {sharesCount viewsCount}comments {uid createdAt updatedAt content author {uid createdAt updatedAt fullName publicName avatarUrl commentsCount}content}}}",
+                    callback: callback)
     }
     
     func requestCurrentFrontPage(callback: @escaping (_ result: Result<CurrentFrontPageResponse, Swift.Error>) -> Void) {
-        var urlComponents = URLComponents()
-        urlComponents.scheme = scheme
-        urlComponents.host = host
-        urlComponents.path = "/gql"
-        guard let url = urlComponents.url else {
-            fatalError()
-        }
-        var urlRequest = URLRequest(url: url)
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
-        urlRequest.addValue("1", forHTTPHeaderField: "DNT")
-        urlRequest.addValue("https://api.razrabs.ru", forHTTPHeaderField: "Origin")
-        urlRequest.httpMethod = "POST"
-        let jsonDictionary = [
-            "query" : "{currentFrontPage {uid createdAt updatedAt title publicationDate endDate content {uid createdAt updatedAt postUid frontPageUid componentUid position {x y} post {uid createdAt updatedAt title previewUrl content description status} component {uid createdAt updatedAt name configuration {h w type}}}}}",
-        ]
-        guard let jsonData = try? JSONSerialization.data(withJSONObject: jsonDictionary, options: []) else {
-            fatalError()
-        }
-        urlRequest.httpBody = jsonData
-        makeRequest(urlRequest: urlRequest, callback: callback)
+        makeRequest(body: "{currentFrontPage {uid createdAt updatedAt title publicationDate endDate content {uid createdAt updatedAt postUid frontPageUid componentUid position {x y} post {uid createdAt updatedAt title previewUrl content description status} component {uid createdAt updatedAt name configuration {h w type}}}}}",
+                    callback: callback)
     }
     
     func requestFeed(callback: @escaping (_ result: Result<FeedResponse, Swift.Error>) -> Void) {
-        var urlComponents = URLComponents()
-        urlComponents.scheme = scheme
-        urlComponents.host = host
-        urlComponents.path = "/gql"
-        guard let url = urlComponents.url else {
-            fatalError()
-        }
-        var urlRequest = URLRequest(url: url)
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
-        urlRequest.addValue("1", forHTTPHeaderField: "DNT")
-        urlRequest.addValue("https://api.razrabs.ru", forHTTPHeaderField: "Origin")
-        urlRequest.httpMethod = "POST"
-        let jsonDictionary = [
-            "query" : "query GetFeeds {feeds {uid name createdAt updatedAt tags {uid createdAt updatedAt name description} status}}",
-        ]
-        guard let jsonData = try? JSONSerialization.data(withJSONObject: jsonDictionary, options: []) else {
-            fatalError()
-        }
-        urlRequest.httpBody = jsonData
-        makeRequest(urlRequest: urlRequest, callback: callback)
+        makeRequest(body: "query GetFeeds {feeds {uid name createdAt updatedAt tags {uid createdAt updatedAt name description} status}}",
+                    callback: callback)
     }
 }
